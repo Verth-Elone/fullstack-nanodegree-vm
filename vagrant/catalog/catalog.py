088b4f272e9f6936c8b30ac2138fc9197e796d60
Verth-Elone <verthelone@gmail.com>
2015-12-16 16:52:38 +0100

    Update catalog.py

--- vagrant/catalog/catalog.py
+++ vagrant/catalog/catalog.py
@@ -9,7 +9,7 @@ db = 'catalog'
 psql_user = 'vagrant'
 psql_pass = 'vagrant'
 conn_str = 'postgresql+psycopg2://{u}:{p}@localhost/catalog'.format(
-	u=psql_user, p=psql_pass)
+    u=psql_user, p=psql_pass)
 
 engine = create_engine(conn_str)
 Base.metadata.bind = engine
@@ -19,38 +19,50 @@ session = DBSession()
 
 @app.route('/')
 @app.route('/index')
-@app.route('/catalog')
+def showMain():
+    return render_template('main.html')
+
+@app.route('/catalog/')
 def showCatalog():
-	categories = session.query(Category).all()
-	items = session.query(Item).all()
-	return render_template('index.html', categories=categories, items=items)
+    categories = session.query(Category).all()
+    items = session.query(Item).all()
+    return render_template('catalog.html', categories=categories, items=items)
 
-@app.route('/catalog/<int:category_id>')
+@app.route('/catalog/c<int:category_id>/')
 def showCategory(category_id):
-	return "page to create show category items"
+    categories = session.query(Category).all()
+    category = session.query(Category).filter_by(id = category_id).one()
+    items = session.query(Item).filter_by(category_id = category_id).all()
+    return render_template('category.html', categories=categories,
+        category=category, items=items)
 
-@app.route('/catalog/<int:category_id>/new', methods=['GET', 'POST'])
-def newCategory(category_id):
-	return "page to crate a new category"
+@app.route('/catalog/category/new/', methods=['GET', 'POST'])
+def newCategory():
+    categories = session.query(Category).all()
+    return render_template('category_new.html')
 
-@app.route('/catalog/<int:category_id>/edit', methods=['GET', 'POST'])
+@app.route('/catalog/<int:category_id>/edit/', methods=['GET', 'POST'])
 def editCategory(category_id):
-	return "page to edit a category"
+    categories = session.query(Category).all()
+    return "page to edit a category"
 
-@app.route('/catalog/<int:category_id>/<int:item_id>')
+@app.route('/catalog/<int:category_id>/<int:item_id>/')
 def showItem(category_id, item_id):
-	return "page to show an item"
+    categories = session.query(Category).all()
+    return "page to show an item"
 
-@app.route('/catalog/<int:category_id>/<int:item_id>/new', methods=['GET', 'POST'])
+@app.route('/catalog/<int:category_id>/<int:item_id>/new/', methods=['GET', 'POST'])
 def newItem(category_id, item_id):
-	return "page to create a new item"
+    categories = session.query(Category).all()
+    return "page to create a new item"
 
-@app.route('/catalog/<int:category_id>/<int:item_id>/edit', methods=['GET', 'POST'])
+@app.route('/catalog/<int:category_id>/<int:item_id>/edit/', methods=['GET', 'POST'])
 def editItem(category_id, item_id):
-	return "page to edit an item"
+    categories = session.query(Category).all()
+    return "page to edit an item"
 
 
 if __name__ == '__main__':
-	# set to False on production version
-	app.debug = True
-	app.run(host = '0.0.0.0', port = 5000)
+    # set to False on production version
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 5000)
